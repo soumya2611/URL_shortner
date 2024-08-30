24236b5a40da17afd90892335ab1e7a34917225d
soumya2611 <workforsoumya21@gmail.com>
2024-08-30 17:17:30 +0530

    env update

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
-
+require('dotenv').config();
 const mongoose = require('mongoose');
 const db = require('./db');
 const userRoutes = require('./routes/userRoutes')
 app.use('/', userRoutes);
-app.listen(3000, () => {
+PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('server running ')
 })
